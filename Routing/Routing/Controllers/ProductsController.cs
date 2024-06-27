f8d03a05bd2e577eedc568e305b9e20a177a66a7
DoanLeee <lentodoan678@gmail.com>
2024-06-27 23:19:48 +0700

    firstcommit
    MVC_Routing

---
+++ Routing/Routing/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routing.Controllers
+{
+    public class ProductsController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        [Route("Products/Details/{id:int?}")]
+        public ActionResult Details(int id)
+        {
+
+            var Product = new[]
+            {
+               new  {ProductId=1,ProductName="SGK",price=15000},
+               new  {ProductId=2,ProductName="Conan",price=15000},
+               new  {ProductId=3,ProductName="Doreamon",price=20000},
+               new  {ProductId=4,ProductName="Tamhon",price=30000}
+
+           };
+            string proName = "";
+            foreach (var product in Product)
+            {
+                if (product.ProductId == id)
+                {
+                    proName = product.ProductName;
+                }
+
+            }
+            ViewBag.ProName = proName;
+            return View();
+        }
+    }
+}

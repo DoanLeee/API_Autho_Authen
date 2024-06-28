400a8e2b5a3108abd8ad738c06383c630b75f987
DoanLeee <lentodoan678@gmail.com>
2024-06-28 13:59:04 +0700

    BackgroundService

---
+++ BackgroundTask/ExampleBgServicecs.cs
@@ -0,0 +1,15 @@
+﻿
+namespace BackgroundTask
+{
+    public class ExampleBgServiec : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+           // while (!stoppingToken.IsCancellationRequested) 
+            {
+                Console.WriteLine("Ping");
+               // await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ BackgroundTask/ExmapleService.cs
@@ -0,0 +1,49 @@
+﻿
+namespace BackgroundTask
+{
+    public class ExmapleService : IHostedLifecycleService
+    {
+        private readonly ILogger<ExmapleService> _logger;
+
+        public ExmapleService(ILogger<ExmapleService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("StarAsync");
+            return Task.CompletedTask;
+        }
+
+        public Task StartedAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("StarAsync");
+            return Task.CompletedTask;
+        }
+
+        public Task StartingAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("StarAsync");
+            return Task.CompletedTask;
+        }
+
+        public  Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("StopAsync");
+            return Task.CompletedTask;
+        }
+
+        public Task StoppedAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("StopAsync");
+            return Task.CompletedTask;
+        }
+
+        public Task StoppingAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("StoppingAsync");
+            return Task.CompletedTask;
+        }
+    }
+}

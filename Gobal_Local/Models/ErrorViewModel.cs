0e14b38818b5ae995420349a4827a610d0cac784
DoanLeee <lentodoan678@gmail.com>
2024-06-28 15:15:10 +0700

    Gobal_Local

---
+++ Gobal_Local/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gobal_Local.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

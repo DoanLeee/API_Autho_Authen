7c14c351e04e4925fcde833cde0534cf3c70866f
DoanLeee <lentodoan678@gmail.com>
2024-06-30 22:49:21 +0700

    firstcommit

---
+++ DI/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+namespace DI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        [HttpGet("")]       
+        
+        public IActionResult GetEmployee([FromKeyedServices("employee")]IEmployee employee)
+        {
+            return Ok(employee.GetMessage());
+        }
+        [HttpGet("temp")]
+        public IActionResult GetTempEmployee([FromKeyedServices("tempemployee")] IEmployee temployee)
+        { 
+            return Ok(temployee.GetMessage());
+        }
+    }
+}
---
+++ DI/IEmployee.cs
@@ -0,0 +1,22 @@
+﻿namespace DI
+{
+    public interface IEmployee
+    {
+        string GetMessage();
+    }
+
+    public class EmploeeRepository : IEmployee
+    {
+        public string GetMessage()
+        {
+            return "Hello form Employess";
+        }
+    }
+    public class TempEmploeeRepository : IEmployee
+    {
+        public string GetMessage()
+        {
+            return "Hello form temp Employess";
+        }
+    }
+}
